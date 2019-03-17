61c87903c6762757f9171c1afc877bb4b2c1edd6
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2019-03-17 13:20:07 +0000

    Threshod and delta frame video

--- capture_video.py
+++ capture_video.py
@@ -7,6 +7,8 @@ Created on Sat Mar 16 18:30:48 2019
 
 import cv2
 
+first_frame = None
+
 #capture the video
 video = cv2.VideoCapture(0)
 
@@ -14,13 +16,31 @@ while True:
     #check if the camera works
     #store the frame values
     check, frame = video.read()
-    print(frame)
     
-    #store the first frame, gray
+    #keep first frame
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0) #removes noise, improves accuracy
+    if first_frame is None:
+        first_frame = gray
+        continue
+    
+    #delta frame
+    delta_frame = cv2.absdiff(first_frame, gray)
+    #white above threshold, black below threshold
+    thresh_frame = cv2.threshold(delta_frame, 20, 255, cv2.THRESH_BINARY)[1]
+    
+    #smooth threshold
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    
+#    #area of countours as tuple
+#    (_, cnts, _) = cv2.findContours(thresh_frame.copy(),
+#     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
     
     #show the frame
-    cv2.imshow("Capturing", gray)
+    cv2.imshow("Gray frame", gray)
+    cv2.imshow("Delta frame", delta_frame)
+    cv2.imshow("Threshold Frame", thresh_frame)
     
     #exit the preview
     key = cv2.waitKey(40)
@@ -31,6 +51,3 @@ while True:
 video.release()
 #close window
 cv2.destroyAllWindows()
-
-
-
---
+++ capture_video2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 10:58:33 2019
+
+@author: Maria
+"""
+
