0072a5be036754edcf31330a84bd04685a944bd7
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2019-03-21 19:32:35 +0000

    Motion detector

--- capture_video.py
+++ capture_video.py
@@ -28,24 +28,33 @@ while True:
     delta_frame = cv2.absdiff(first_frame, gray)
     #white above threshold, black below threshold
     thresh_frame = cv2.threshold(delta_frame, 20, 255, cv2.THRESH_BINARY)[1]
-    
-    #smooth threshold
+    #smooth threshold frame
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
     
-#    #area of countours as tuple
-#    (_, cnts, _) = cv2.findContours(thresh_frame.copy(),
-#     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     
+    #retrieve countours as tuple
+    (cnts, _) = cv2.findContours(thresh_frame.copy(), 
+     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    #keep countours above 100pxls
+    for contour in cnts:
+        if cv2.contourArea(contour) < 8000:
+            continue
+        #else draw rectangle
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x, y), (x+w, y+h), (0, 255, 0), 3)
+        
     #show the frame
     cv2.imshow("Gray frame", gray)
     cv2.imshow("Delta frame", delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Colour Frame", frame)
     
     #exit the preview
     key = cv2.waitKey(40)
     if key == ord('q'):
         break
+
 
 #release the video
 video.release()
